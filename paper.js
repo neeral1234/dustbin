f832cccd2cae52b0c2534fcec657ed35a529ec73
neeral1234 <76726903+neeral1234@users.noreply.github.com>
2021-07-13 13:57:38 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,34 @@
+class paper
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+            restitution:0.3,
+            friction:0,
+            density:1.2			
+			}
+		this.x=x;
+		this.r=r;
+        this.y=y;
+        this.Image=loadImage("paper.png")
+		this.body=Bodies.circle(this.x,this.y, (this.r-20)/2 , options);
+ 		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var paperPos=this.body.position;		
+
+			push()
+			translate(paperPos.x, paperPos.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			fill(128,128,128)
+			image(this.Image,0,0,this.r, this.r);
+			pop()
+			
+	}
+
+}
